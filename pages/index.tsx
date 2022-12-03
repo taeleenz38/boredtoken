5838e1a195d145e3caea89572b45a27f98cc3881
Tae Lee <taekyu.lee3@gmail.com>
2022-12-04 07:17:45 +1100

    responsivity WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className='hide-scrollbar text-white bg-black h-screen'>
+    <div className='hide-scrollbar text-white bg-black h-screen w-screen'>
 
       <Head>
         <title>$BORED</title>
@@ -13,49 +13,59 @@ export default function Home() {
       </Head>
 
       <div className='hide-scrollbar scroll-smooth boxheight overflow-scroll'>
-        <div className='Section1 h-full w-full'>
+        <div className='Section1 h-full w-full' id='home'>
           <div className='h-full w-full flex items-center'>
-            <img className="w-9/12 mx-auto rounded-full" src="/APE.png" alt="logo" />
+            <img className="w-9/12 mx-auto rounded-full lg:w-4/12" src="/APE.png" alt="logo" />
           </div>
         </div>
 
         <div className='Section2 h-full w-full'>
-          <div className='h-full w-full py-1'>
-            <div className='w-11/12 h-40 mx-auto bg-primary/25 my-5 rounded-lg p-2 font-extralight shadow-sm shadow-primary'>Roadmap phase 1</div>
-            <div className='w-11/12 h-40 mx-auto bg-primary/25 my-5 rounded-lg p-2 font-extralight shadow-sm shadow-primary'>Roadmap phase 2</div>
-            <div className='w-11/12 h-40 mx-auto bg-primary/25 my-5 rounded-lg p-2 font-extralight shadow-sm shadow-primary'>Roadmap phase 3</div>
-            <div className='w-11/12 h-40 mx-auto bg-primary/25 my-5 rounded-lg p-2 font-extralight shadow-sm shadow-primary'>Roadmap phase 4</div>
+          <div className='h-full w-full py-1 lg:flex lg:items-center lg:w-10/12 lg:mx-auto'>
+            <div className='w-11/12 h-40 mx-auto bg-primary/25 my-5 rounded-lg p-2 font-extralight shadow-sm shadow-primary lg:w-1/5 lg:h-3/6 lg:my-0 lg:mb-8 lg:shadow-lg lg:shadow-primary'>Phase 1</div>
+            <div className='w-11/12 h-40 mx-auto bg-primary/25 my-5 rounded-lg p-2 font-extralight shadow-sm shadow-primary lg:w-1/5 lg:h-3/6 lg:my-0 lg:mb-8 lg:shadow-lg lg:shadow-primary'>Phase 2</div>
+            <div className='w-11/12 h-40 mx-auto bg-primary/25 my-5 rounded-lg p-2 font-extralight shadow-sm shadow-primary lg:w-1/5 lg:h-3/6 lg:my-0 lg:mb-8 lg:shadow-lg lg:shadow-primary'>Phase 3</div>
+            <div className='w-11/12 h-40 mx-auto bg-primary/25 my-5 rounded-lg p-2 font-extralight shadow-sm shadow-primary lg:w-1/5 lg:h-3/6 lg:my-0 lg:mb-8 lg:shadow-lg lg:shadow-primary'>Phase 4</div>
           </div>
         </div>
 
         <div className='Section3 bg-primary/25 h-full w-full items-center'>
-          <h1 className='text-center font-semibold py-8 text-3xl'>Tokenomics</h1>
-          <li className='px-12 my-1 text-md'>info</li>
-          <li className='px-12 my-1 text-md'>info</li>
-          <li className='px-12 my-1 text-md'>info</li>
-          <li className='px-12 my-1 text-md'>info</li>
-          <li className='px-12 my-1 text-md'>info</li>
-          <li className='px-12 my-1 text-md'>info</li>
-          <img className="w-7/12 mx-auto rounded-full my-20" src="/APE.png" alt="logo" />
-          <p className='w-full text-center text-sm' value="0xD9F6724F250F4dA8453e3DD97d718872BF72C1Da">0xD9F6724F250F4dA8453e3DD97d718872BF72C1Da</p>
-          <div className='w-full flex justify-center items-center mt-4'>
-            <button className='border-white border-2 py-2 px-5 rounded-lg font-extralight text-center shadow-lg shadow-primary' onClick={() => navigator.clipboard.writeText('0xD9F6724F250F4dA8453e3DD97d718872BF72C1Da')}>Copy to clipboard</button>
+          <h1 className='text-center font-semibold py-8 text-3xl lg:hidden'>Tokenomics</h1>
+          <div className='lg:flex lg:w-9/12 xl:w-7/12 lg:h-full lg:items-center lg:justify-between lg:mx-auto'>
+            <div>
+              <h1 className='font-semibold text-7xl hidden lg:block mb-5'>Tokenomics</h1>
+              <div className='lg:mb-32'>
+                <li className='px-12 w-8/12 mx-auto text-sm my-1 text-md lg:px-0 lg:w-10/12'>info</li>
+                <li className='px-12 w-8/12 mx-auto text-sm my-1 text-md lg:px-0 lg:w-10/12'>info</li>
+                <li className='px-12 w-8/12 mx-auto text-sm my-1 text-md lg:px-0 lg:w-10/12'>info</li>
+                <li className='px-12 w-8/12 mx-auto text-sm my-1 text-md lg:px-0 lg:w-10/12'>info</li>
+                <li className='px-12 w-8/12 mx-auto text-sm my-1 text-md lg:px-0 lg:w-10/12'>info</li>
+                <li className='px-12 w-8/12 mx-auto text-sm my-1 text-md lg:px-0 lg:w-10/12'>info</li>
+              </div>
+            </div>
+            <div className='lg:mb-16'>
+              <img className="w-6/12 mx-auto rounded-full my-20 sm:w-60 md:w-80 md:my-10" src="/APE.png" alt="logo" />
+              <p className='w-full text-center text-sm' value="0xD9F6724F250F4dA8453e3DD97d718872BF72C1Da">0xD9F6724F250F4dA8453e3DD97d718872BF72C1Da</p>
+              <div className='w-full flex justify-center items-center mt-4'>
+                <button className='border-white border-2 py-2 px-5 rounded-lg font-extralight text-center shadow-lg shadow-primary transition-all duration-300 hover:bg-white hover:text-black hover:border-black' onClick={() => navigator.clipboard.writeText('0xD9F6724F250F4dA8453e3DD97d718872BF72C1Da')}>Copy to clipboard</button>
+              </div>
+            </div>
           </div>
         </div>
 
         <div className='Section4 h-full w-full flex flex-col justify-center items-center pt-12'>
-          <h1 className='text-center font-semibold text-5xl'>JOIN US</h1>
+          <h1 className='text-center font-semibold text-5xl lg:text-8xl'>JOIN US</h1>
           <Link href="#" target="_blank">
-            <h2 className=" border-white border-2 text-lg font-medium py-2 px-5 rounded-lg mt-5 text-white hover:bg-white hover:text-black hover:border-black shadow-lg shadow-primary">Buy Now</h2>
+            <h2 className=" border-white border-2 text-lg font-medium py-2 px-5 rounded-lg mt-5 text-white transition-all duration-300 hover:bg-white hover:text-black hover:border-black shadow-lg shadow-primary lg:text-2xl lg:py-3 lg:px-8 lg:mt-8">Buy Now</h2>
           </Link>
           <div className='mt-10'>socials (icons)</div>
         </div>
       </div>
 
-
-      <footer className="w-11/12 mx-auto pt-2 left-4 bottom-4 font-medium border-t-2 border-t-white fixed flex justify-between">
-        <p>Bored Token</p>
-        <p>Menu</p>
+      <footer className="wrap w-11/12 mx-auto">
+        <footer className="w-11/12 pt-2 bottom-4 font-medium border-t-2 border-t-white fixed flex justify-between lg:bottom-6 lg:pt-4">
+          <p>Bored Token</p>
+          <Link href="#home">Home</Link>
+        </footer>
       </footer>
 
     </div >
