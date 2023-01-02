a22ef416b102552d9da75b6e5abdb3773ba1b718
Tae Lee <taekyu.lee3@gmail.com>
2023-01-03 02:22:13 +1100

    assets updated

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,7 +22,7 @@ export default function Home() {
       </Head>
 
       <div className='h-screen w-screen hide-scrollbar scroll-smooth overflow-scroll'>
-        <div className='Section1 h-full w-11/12 mx-auto pb-14' id='home'>
+        <div className='Section1  h-full w-11/12 mx-auto pb-14' id='home'>
           <div className='h-full w-full flex flex-col justify-center items-center'>
             <video preload='auto' playsInline loop autoPlay muted className='w-full md:w-2/3' >
               <source src="/boredbig.mp4" type='video/mp4' />
@@ -41,9 +41,9 @@ export default function Home() {
           </div>
         </div>
 
-        <div className='Section2 h-full w-11/12 mx-auto border-b-2 my-0 pb-14'>
+        <div className='Section2 bg-primary/10 h-full w-full mx-auto my-0 pb-14'>
           <div className='h-full w-full py-1 lg:flex lg:items-center lg:w-full xl:w-10/12 lg:mx-auto'>
-            <div className='w-11/12 h-1/5 mx-auto bg-black border-2 my-5 rounded-lg p-2 lg:p-4 font-extralight shadow-md shadow-primary lg:w-1/5 lg:h-2/5 xl:h-1/2 lg:my-0 lg:mb-8 lg:shadow-xl lg:shadow-primary transition-all duration-200 lg:hover:-translate-y-2 overflow-y-hidden'>
+            <div className='w-10/12 h-1/5 mx-auto bg-black border-2 my-5 rounded-lg p-2 lg:p-4 font-extralight shadow-md shadow-primary lg:w-1/5 lg:h-2/5 xl:h-1/2 lg:my-0 lg:mb-8 lg:shadow-xl lg:shadow-primary transition-all duration-200 lg:hover:-translate-y-2 overflow-y-hidden'>
               <h1 className='text-lg font-semibold lg:text-2xl mb-2 lg:mb-5 py-1 border-b'>Phase 1</h1>
               <p className='text-md font-extralight lg:mb-1'>- Token launch</p>
               <p className='text-md lg:mb-1 font-extralight'>- Buy BAYC/MAYC + $APE</p>
@@ -53,7 +53,7 @@ export default function Home() {
                 </video>
               </div>
             </div>
-            <div className='w-11/12 h-1/5 mx-auto bg-black border-2 my-5 rounded-lg p-2 lg:p-4 font-extralight shadow-md shadow-primary lg:w-1/5 lg:h-2/5 xl:h-3/6 lg:my-0 lg:mb-8 lg:shadow-xl lg:shadow-primary transition-all duration-200 lg:hover:-translate-y-2 overflow-y-hidden'>
+            <div className='w-10/12 h-1/5 mx-auto bg-black border-2 my-5 rounded-lg p-2 lg:p-4 font-extralight shadow-md shadow-primary lg:w-1/5 lg:h-2/5 xl:h-3/6 lg:my-0 lg:mb-8 lg:shadow-xl lg:shadow-primary transition-all duration-200 lg:hover:-translate-y-2 overflow-y-hidden'>
               <h1 className='text-lg font-semibold lg:text-2xl mb-2 lg:mb-5 py-1 border-b'>Phase 2</h1>
               <p className='text-md font-extralight lg:mb-1'>- $APE staking rewards</p>
               <p className='text-md lg:mb-1 font-extralight'>- Start marketing phase</p>
@@ -64,7 +64,7 @@ export default function Home() {
               </div>
 
             </div>
-            <div className='w-11/12 h-1/5 mx-auto bg-black border-2 my-5 rounded-lg p-2 lg:p-4 font-extralight shadow-md shadow-primary lg:w-1/5 lg:h-2/5 xl:h-3/6 lg:my-0 lg:mb-8 lg:shadow-xl lg:shadow-primary transition-all duration-200 lg:hover:-translate-y-2 overflow-y-hidden'>
+            <div className='w-10/12 h-1/5 mx-auto bg-black border-2 my-5 rounded-lg p-2 lg:p-4 font-extralight shadow-md shadow-primary lg:w-1/5 lg:h-2/5 xl:h-3/6 lg:my-0 lg:mb-8 lg:shadow-xl lg:shadow-primary transition-all duration-200 lg:hover:-translate-y-2 overflow-y-hidden'>
               <h1 className='text-lg font-semibold lg:text-2xl mb-2 lg:mb-5 py-1 border-b'>Phase 3</h1>
               <p className='text-md  font-extralight lg:mb-1'>- MAYC giveaway</p>
               <p className='text-md lg:mb-1 font-extralight'>- Tax used to purchase $APE</p>
@@ -72,7 +72,7 @@ export default function Home() {
                 <img src="/apecoin.gif" className='w-9/12 rounded-full hidden 2xl:block mx-auto' alt="apecoin" />
               </div>
             </div>
-            <div className='w-11/12 h-1/5 mx-auto bg-black border-2 my-5 rounded-lg p-2 lg:p-4 font-extralight shadow-md shadow-primary lg:w-1/5 lg:h-2/5 xl:h-3/6 lg:my-0 lg:mb-8 lg:shadow-xl lg:shadow-primary transition-all duration-200 lg:hover:-translate-y-2 overflow-y-hidden'>
+            <div className='w-10/12 h-1/5 mx-auto bg-black border-2 my-5 rounded-lg p-2 lg:p-4 font-extralight shadow-md shadow-primary lg:w-1/5 lg:h-2/5 xl:h-3/6 lg:my-0 lg:mb-8 lg:shadow-xl lg:shadow-primary transition-all duration-200 lg:hover:-translate-y-2 overflow-y-hidden'>
               <h1 className='text-lg font-semibold lg:text-2xl mb-2 lg:mb-5 py-1 border-b'>Phase 4</h1>
               <p className='text-md  font-extralight lg:mb-1'>- Platform for ownership of all bored dao staking assets</p>
               <div className='hidden lg:h-40 xl:h-60 lg:flex lg:justify-center lg:items-center'>
@@ -81,6 +81,33 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+
+        <div className='Section4 h-full w-11/12 mx-auto border-b-2 sm:flex sm:items-center overflow-y-hidden my-0'>
+          <h1 className='font-semibold mt-2 sm:mt-4 sm:ml-10 sm:text-4xl'>Staked NFTs</h1>
+          <div className='h-full w-full sm:hidden overflow-y-scroll hide-scrollbar pb-14'>
+            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="BAYC1.jpg" alt="bayc1" />
+            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="mutant 1.jpg" alt="mutant1" />
+            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="MAYC1007.jpg" alt="mutant2" />
+            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="mayc 28882.jpg" alt="mutant3" />
+
+            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="bakc 1281.jpg" alt="bakc4" />
+            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="bakc1.png" alt="bakc1" />
+            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="bakc 6791.jpg" alt="bakc2" />
+            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="BAKC638.jpg" alt="bakc3" />
+          </div>
+          <div className='items hidden w-full sm:flex overflow-x-auto cursor-grab py-2 scrollbar'>
+            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="BAYC1.jpg" alt="bayc1" />
+            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="mutant 1.jpg" alt="mutant1" />
+            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="MAYC1007.jpg" alt="mutant2" />
+            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="mayc 28882.jpg" alt="mutant3" />
+            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="bakc 1281.jpg" alt="bakc4" />
+            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="bakc1.png" alt="bakc1" />
+            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="bakc 6791.jpg" alt="bakc2" />
+            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="BAKC638.jpg" alt="bakc3" />
+          </div>
+        </div>
+
 
         <div className='Section3 bg-primary/10 h-full w-full items-center my-0 pt-5 pb-14'>
           <div className='flex flex-col h-full w-full lg:w-8/12 mx-auto lg:flex-row py-8 lg:py-8 justify-between lg:items-center'>
@@ -113,25 +140,7 @@ active:border-black lg:hover:bg-white lg:hover:text-black lg:hover:border-black'
           </div>
         </div>
 
-        <div className='Section4 h-full w-11/12 mx-auto border-t-2 border-b-2 sm:flex sm:items-center overflow-y-hidden my-0'>
-          <h1 className='font-semibold mt-2 sm:mt-4 sm:ml-10 sm:text-4xl'>Staked NFTs</h1>
-          <div className='h-full w-full sm:hidden overflow-y-scroll hide-scrollbar pb-14'>
-            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="BAYC1.jpg" alt="bayc1" />
-            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="mutant 1.jpg" alt="mutant1" />
-            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="MAYC1007.jpg" alt="mutant2" />
-            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="bakc1.png" alt="bakc1" />
-            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="bakc 6791.jpg" alt="bakc2" />
-            <img className='w-11/12 mx-auto my-2 rounded-2xl' src="BAKC638.jpg" alt="bakc3" />
-          </div>
-          <div className='items hidden w-full sm:flex overflow-x-auto cursor-grab py-2 scrollbar'>
-            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="BAYC1.jpg" alt="bayc1" />
-            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="mutant 1.jpg" alt="mutant1" />
-            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="MAYC1007.jpg" alt="mutant2" />
-            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="bakc1.png" alt="bakc1" />
-            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="bakc 6791.jpg" alt="bakc2" />
-            <img className='sm:w-5/12 mr-3 rounded-2xl inline-block' src="BAKC638.jpg" alt="bakc3" />
-          </div>
-        </div>
+
 
 
         <div className='Section5 h-full w-full flex flex-col justify-center items-center pb-14' id="socials">
